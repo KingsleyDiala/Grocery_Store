5e984c0e3d5c168df99c2097201f012a2d622866
kingsley Diala <donbuski2@gmail.com>
2022-05-13 18:38:05 +0200

    Added animation

--- src/resources/components/Reusable_Components/Full_Card.jsx
+++ src/resources/components/Reusable_Components/Full_Card.jsx
@@ -135,7 +135,7 @@ const FullCard = ({ index, cart, setCart, item, wishlist, setWishlist }) => {
     <ItemContainer key={index}>
               <ImageContainer className="image__container">
                 <Link to={`/product/${item.id}`}>
-                  { item.image ? <Image src={item.image} alt={item.name} /> : <Skeleton
+                  { item.image ? <Image src={item.image} alt={item.name} /> : <Skeleton animation='wave'
         sx={{ bgcolor: 'grey.900' }}
         variant="rectangular"
         width={300}

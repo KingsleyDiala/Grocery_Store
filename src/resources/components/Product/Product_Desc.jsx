edf93281dd6e6a843d03e8ca2b4bd80496314b26
kingsley Diala <donbuski2@gmail.com>
2022-05-14 08:16:15 +0200

    added new arcodion

--- src/resources/components/Product/Product_Desc.jsx
+++ src/resources/components/Product/Product_Desc.jsx
@@ -1,4 +1,12 @@
 import styled from "styled-components";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { fontWeight } from "@mui/system";
+
 
 
 //   =============== STYLED COMPONENT ==================
@@ -102,22 +110,87 @@ border-radius: 10px;
 const RightContainer = styled.div`
 flex: 2.5;
 `
-const Text = styled.p`
-text-align: justify;
-`
+const Text = {
+  textAlign: 'justify',
+  fontWeight: '400',
+}
 
 
 
+// ================ DESCRIPTION ================
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 4 }}>
+          <Typography >{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function BasicTabs({ product }) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Description" {...a11yProps(0)} />
+          <Tab label="Nutritional Facts" {...a11yProps(1)} />
+          <Tab label="Features" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <p style={Text}>{product.generatedText}</p>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Box>
+  );
+}
 
 
 
 //   =============== JSX STRUCTURE COMPONENT ==================
 
 export const ProductDescriptionComponent = ({ product }) => {
+  console.log(product);
   return (
     <ProductDescription>
       <Container>
-        <LeftContainer>
+        <BasicTabs product={product} />
+        {/* <LeftContainer>
           <Description>Description</Description>
           <NutritionFacts>Nutrition Facts</NutritionFacts>
           <Features>Features</Features>
@@ -127,7 +200,7 @@ export const ProductDescriptionComponent = ({ product }) => {
           <Text>
           {product.generatedText}
           </Text>
-        </RightContainer>
+        </RightContainer> */}
       </Container>
     </ProductDescription>
   )
